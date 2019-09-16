14e55525e16e9e920d675d4fa3faea6553470973
Sodif <psaidwid@flowgaming.org>
2019-09-16 02:50:18 -0400

    Adding user creation and increasing security of rank changing

--- api.js
+++ api.js
@@ -49,13 +49,13 @@ var schema = buildSchema(`
   }
 
   type User{
-    rank: Int
+    rank: String
     username: String
     uniqueid: String
     email: String
     discordName: String
     discordId: String
-    accountStatus: Int
+    accountStatus: String
     ipList: [String]
     pc_hwid: String
     access: Access
@@ -83,10 +83,14 @@ var schema = buildSchema(`
 
   type Query {
     getUser(search: SearchType!, id: String!): User
+
+    getAllUsers(serverToken: String): [User]
   }
 
   type Mutation {
     editUser(uniqueid: String!, field: FieldType!, data: String!): Boolean
+
+    createUser(username: String!, email: String!, discordId: String!, discordName: String!, ip: String!): Boolean
   }
 `);
 
@@ -98,8 +102,17 @@ var root = {
     return getUser({type: search.search, identifier: search.id}, context);
   },
 
-  editUser: function (user, context){
+  getAllUsers: function (token, context) {
+    if (token.length != null) return getAllUsers(sanitizeString(token), context)
+    else return getAllUsers(sanitizeString(context.req.cookies.id), context)
+  },
+
+  editUser: function (user, context) {
     return editUser(user, context);
+  },
+
+  createUser: function (user, context) {
+    return createUser({username: user.username, email: user.email, discordId: user.discordId, discordName: user.discordName, ip: user.ip}, context);
   }
 };
 
@@ -114,8 +127,6 @@ app.use('/graphql', graphqlHTTP(function(req, res, params) {
     context: { req, res, params }
   }
 }));
-
-
 
 //=========================== Functions =============================
 
@@ -212,6 +223,30 @@ function getUser (search, context) {
   });
 }
 
+function getAllUsers(token, context) {
+  return new Promise((resolve, reject) => {
+    console.log(token);
+    isAuthOrAdmin(token, passwords.serverIdToken).then((isUserAuthed) => {
+      if (isUserAuthed) {
+
+        console.log("getAllUsers() from "+ sanitizeString(context.req.cookies.id));
+        var cUsers = dbo.collection("users");
+
+        cUsers.find({username: {$ne: "server"}}).toArray(function(err, users) {
+          if (err) reportError(reject, err);
+
+          resolve(users);
+        });
+      } else {
+        reportError(reject, "User Not Authorized to access this Data");
+      }
+    });
+  }).catch((error) => {
+    console.log(error);
+    return false;
+  });
+}
+
 function editUser (user, context) {
   return new Promise((resolve, reject) => {
     isAuthOrMod(context.req.cookies.id, user.uniqueid).then((isUserAuthed) => {
@@ -234,45 +269,57 @@ function editUser (user, context) {
 
         switch (user.field) {
           case FieldType.rank:
-            cUsers.updateOne({uniqueid: sanitizeString(user.uniqueid)}, {$set: { rank: user.data}}, function(err, commandResult) {
-              console.log(commandResult.result);
-              resolve(true);
+            cUsers.findOne({uniqueid: sanitizeString(context.req.cookies.id)}, function(err, myUserData) {
+              if ((parseInt(myUserData.rank) >= Ranks.Admin) && (parseInt(myUserData.rank) > parseInt(user.data))) {
+                cUsers.updateOne({uniqueid: sanitizeString(user.uniqueid)}, {$set: {rank: user.data}}, function(err, commandResult) {
+                  console.log(commandResult.result);
+                  resolve(true);
+                });
+              } else {
+                reportError(reject, "User Not Authorized to change this Data");
+              }
             });
             break;
           case FieldType.username:
-            cUsers.updateOne({uniqueid: sanitizeString(user.uniqueid)}, {$set: { username: user.data}}, function(err, commandResult) {
+            cUsers.updateOne({uniqueid: sanitizeString(user.uniqueid)}, {$set: { username: sanitizeString(user.data)}}, function(err, commandResult) {
               console.log(commandResult.result);
               resolve(true);
             });
             break;
           case FieldType.uniqueid:
-            cUsers.updateOne({uniqueid: sanitizeString(user.uniqueid)}, {$set: { uniqueid: user.data}}, function(err, commandResult) {
+            cUsers.updateOne({uniqueid: sanitizeString(user.uniqueid)}, {$set: { uniqueid: sanitizeString(user.data)}}, function(err, commandResult) {
               console.log(commandResult.result);
               resolve(true);
             });
             break;
           case FieldType.email:
-            cUsers.updateOne({uniqueid: sanitizeString(user.uniqueid)}, {$set: { email: user.data}}, function(err, commandResult) {
+            cUsers.updateOne({uniqueid: sanitizeString(user.uniqueid)}, {$set: { email: sanitizeString(user.data)}}, function(err, commandResult) {
               console.log(commandResult.result);
               resolve(true);
             });
             break;
           case FieldType.discordId:
-            cUsers.updateOne({uniqueid: sanitizeString(user.uniqueid)}, {$set: { discordId: user.data}}, function(err, commandResult) {
+            cUsers.updateOne({uniqueid: sanitizeString(user.uniqueid)}, {$set: { discordId: sanitizeString(user.data)}}, function(err, commandResult) {
               console.log(commandResult.result);
               resolve(true);
             });
             break;
           case FieldType.discordName:
-            cUsers.updateOne({uniqueid: sanitizeString(user.uniqueid)}, {$set: { discordName: user.data}}, function(err, commandResult) {
+            cUsers.updateOne({uniqueid: sanitizeString(user.uniqueid)}, {$set: { discordName: sanitizeString(user.data)}}, function(err, commandResult) {
               console.log(commandResult.result);
               resolve(true);
             });
             break;
           case FieldType.accountStatus:
-            cUsers.updateOne({uniqueid: sanitizeString(user.uniqueid)}, {$set: { accountStatus: user.data}}, function(err, commandResult) {
-              console.log(commandResult.result);
-              resolve(true);
+            cUsers.findOne({uniqueid: sanitizeString(user.uniqueid)}, function(err, userData) {
+              if (userData.rank >= Ranks.Admin) {
+                cUsers.updateOne({uniqueid: sanitizeString(user.uniqueid)}, {$set: { accountStatus: sanitizeString(user.data)}}, function(err, commandResult) {
+                  console.log(commandResult.result);
+                  resolve(true);
+                });
+              } else {
+                reportError(reject, "User Not Authorized to change this Data");
+              }
             });
             break;
           case FieldType.ipList:
@@ -295,6 +342,44 @@ function editUser (user, context) {
           default:
             reportError(reject, "Invalid Field Type");
         }
+      } else {
+        reportError(reject, "User Not Authorized to access this Data");
+      }
+    });
+  }).catch((error) => {
+    console.log(error);
+    return false;
+  });
+}
+
+function createUser (user, context) {
+  return new Promise((resolve, reject) => {
+    isAuthOrAdmin(context.req.cookies.id, passwords.serverIdToken).then((isUserAuthed) => {
+      if (isUserAuthed) {
+        var cUsers = dbo.collection("users");
+
+        var newUser = {
+          rank: '0',
+          username: user.username,
+          uniqueid: uuidv4(),
+          email: user.email,
+          discordId: user.discordId,
+          discordName: user.discordName,
+          accountStatus: '0',
+          ipList: [user.ip],
+          pc_hwid: '',
+          access: {}
+        };
+
+        cUsers.insertOne(newUser, function (err, writeStatus) {
+          if (writeStatus) {
+            console.log('createUser("'+ newUser.username +'", "'+ newUser.discordId +'", "'+ user.ip +'")');
+            resolve(true);
+          } else {
+            console.log(writeStatus);
+            reportError(reject, "There was an error creating the user");
+          }
+        });
       } else {
         reportError(reject, "User Not Authorized to access this Data");
       }
@@ -381,13 +466,14 @@ const APIStatus = {
 }
 
 const Ranks = {
-  Guest: 0,
-  Regular: 1,
-  Veteran: 2,
-  Moderator: 3,
-  Developer: 4,
-  Admin: 5,
-  Owner: 6
+  Guest: '0',
+  Newbie: '1',
+  Regular: '2',
+  Veteran: '3',
+  Moderator: '4',
+  Developer: '5',
+  Admin: '6',
+  Owner: '7'
 }
 
 const SearchTypes = {
